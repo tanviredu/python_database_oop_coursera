91414a3d1629901df574082ceb7dd58332b9ca4b
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-03-24 04:14:18 +0600

    added

---
+++ relational_database/database/CREATING_WITH_PYTHON/test2/emaildb.py
@@ -0,0 +1,69 @@
+## importing sqlite database
+import sqlite3
+conn = sqlite3.connect("emaildb.sqlite")
+
+
+### create the cursor
+### cursor is like the computer cursor it does all the things
+
+cur = conn.cursor()
+
+### drop all the table
+
+cur.execute("DROP TABLE IF EXISTS Counts")
+
+### now make a scchema
+cur.execute("CREATE TABLE Counts(org TEXT,count INTEGER)")
+
+
+### Create a file 
+fname = input("Enter The file name : ")
+
+## check if it is the valid name or make a default one
+## or user just press enter
+if(len(fname) < 1):
+    fname = "mbox.txt"
+
+## make a file handles like the cursor fo the file
+
+fh = open(fname)
+
+for line in fh:
+    ## first check if we reached at the end of the file
+    if not line.startswith("From: "): continue
+
+    ### so in the file the line shoud be like "From <email>"
+    ### so we split the line and take the second part
+    pieces = line.split()
+    email = pieces[1]
+
+
+    ## now we only need the organisation name
+    parts = email.split('@')
+    org = (parts[1])
+    ## now enter into the database
+    ## but first we need to find if it is already in the database
+    ## if not create one and if it is then increase the count
+    ## remember the "?"  and the "," after the email
+    ## you have to give it to it
+    cur.execute("SELECT count FROM Counts WHERE org = ? ",(org,)) 
+    ### now fetch the data
+    row = cur.fetchone()
+    ## if there is no data
+    if row is None:
+        ## then insert the data
+        cur.execute("INSERT INTO Counts (org,count) VALUES (?,1)",(org,))
+    else:
+        cur.execute("UPDATE Counts SET count = count + 1 WHERE org = ? ",(org,))
+    conn.commit()
+
+
+## now fetch all the data
+
+sqlstr = "SELECT org,count FROM Counts ORDER BY count DESC LIMIT 10"
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]),row[1])
+
+
+cur.close()
