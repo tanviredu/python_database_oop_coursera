5b2f4da397552f9bfc4b30accd61455ce7bf4420
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-03-26 02:39:27 +0600

    added

---
+++ relational_database/three/final_proj/geodata/where.js
@@ -0,0 +1,9 @@
+myData = [
+[22.3726523,91.80727519999999, 'Abdullah Al Noman Road, Noman Society, East Nasirabad Khulshi Chittagong, Chittagong 4209, Bangladesh'],
+[22.2352023,91.7913938, 'Patenga Sea Beach, Chittagong, Bangladesh'],
+[40.8007046,-73.965088, '949 Amsterdam Ave, New York, NY 10025, USA'],
+[22.2696702,91.7812707, 'TSP Colony, Chittagong, Bangladesh'],
+[22.3665177,91.8228798, '2 No. Gate Bus Stop, Chittagong, Bangladesh'],
+[23.810332,90.4125181, 'Dhaka, Bangladesh'],
+[23.7075811,90.45377479999999, 'Shonir Akhra, Matuail, Bangladesh']
+];
